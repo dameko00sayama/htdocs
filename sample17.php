35c17509ba8398e06ea02c9293c377eb35b6dad9
dameko <nrtk.emerald503@gmail.com>
2021-12-14 08:47:34 +0900

    create

--- sample17.php
+++ sample17.php
@@ -2,7 +2,7 @@
 $xmlTree = simplexml_load_file('data/rss.xml');
 foreach ($xmlTree->channel->item as $item):
 ?>
-.<a href = "<?php echo $item->link ?>"<?php echo $item->title; ?><br>
+.<a href = "<?php echo $item->link; ?>"> ?>"<?php echo $item->title; ?><br>
 <?php
 endforeach;
 ?>
---
+++ sample18.php
@@ -0,0 +1,13 @@
+<?php
+$file = file_get_contents('data/feed.json');
+$json = json_decode($file);
+
+foreach($json->items as $item):
+?>
+
+.<a href = "<?php echo $item->link; ?>"><?php echo $item->title ;?><br>
+
+
+<?php
+endforeach
+?>
